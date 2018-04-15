901d18fff17b93da32fa889de8ad50fce2cbbca4
Rico A. Beti <rico.beti@silentbyte.com>
2018-04-16 03:16:45 +0800

    Add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -12,60 +12,53 @@ const watch = require('gulp-watch');
 
 gulp.task('html', (cb) => {
     pump([
-            gulp.src('./assets/index.html'),
-            htmlmin({
-                collapseWhitespace: true,
-            }),
-            gulp.dest('./templates/'),
-        ],
-        cb,
-    );
+        gulp.src('./assets/index.html'),
+        htmlmin({
+            collapseWhitespace: true,
+        }),
+        gulp.dest('./templates/'),
+    ], cb);
 });
 
 gulp.task('stylus', (cb) => {
     pump([
-            gulp.src('./assets/stylus/rebounce.styl'),
-            stylus({
-                compress: true,
-            }),
-            gulp.dest('./public/assets/css/'),
-        ],
-        cb,
-    );
+        gulp.src('./assets/stylus/rebounce.styl'),
+        stylus({
+            compress: true,
+        }),
+        gulp.dest('./public/assets/css/'),
+    ], cb);
 });
 
 gulp.task('bootstrap', (cb) => {
     pump([
-            gulp.src('./assets/deps/css/bootstrap.min.css'),
-            gulp.dest('./public/assets/css/'),
-        ],
-        cb,
-    );
+        gulp.src('./assets/deps/css/bootstrap.min.css'),
+        gulp.dest('./public/assets/css/'),
+    ], cb);
 });
 
 gulp.task('js', (cb) => {
     pump([
-            gulp.src([
-                './assets/deps/js/jquery.min.js',
-                './assets/deps/js/bootstrap.bundle.min.js',
-                './assets/deps/js/jquery.easing.min.js',
-                './assets/js/rebounce.js',
-            ]),
-            uglify(),
-            concat('rebounce.js'),
-            gulp.dest('./public/assets/js/'),
-        ],
-        cb,
-    );
+        gulp.src([
+            './assets/deps/js/jquery.min.js',
+            './assets/deps/js/bootstrap.bundle.min.js',
+            './assets/deps/js/jquery.easing.min.js',
+            './assets/js/rebounce.js',
+        ]),
+        uglify(),
+        concat('rebounce.js'),
+        gulp.dest('./public/assets/js/'),
+    ], cb);
 });
 
 gulp.task('images', (cb) => {
     pump([
-            gulp.src('./assets/visuals/brand.png'),
-            gulp.dest('./public/assets/images/'),
-        ],
-        cb,
-    );
+        gulp.src('./assets/visuals/favicon.ico'),
+        gulp.dest('./public/'),
+
+        gulp.src('./assets/visuals/brand.png'),
+        gulp.dest('./public/assets/images/'),
+    ], cb);
 });
 
 gulp.task('build', [

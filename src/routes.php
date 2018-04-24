deead34910ea83771e7acc35033e582ea1e7a203
Rico A. Beti <rico.beti@silentbyte.com>
2018-04-24 18:54:21 +0800

    Add echo route

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 
 $app->get('/', function(Request $request, Response $response, array $args) {
     return $this->view->render($response, 'index.html', [
-        'title' => 'Rebounce • Online'
+        'title' => 'Rebounce • Online',
     ]);
 });
 
@@ -15,7 +15,21 @@ $app->get('/', function(Request $request, Response $response, array $args) {
 $app->get('/ip', function(Request $request, Response $response, array $args) {
     $ip = $request->getServerParam('REMOTE_ADDR');
     return $response->withJson([
-        'ip' => $ip
+        'ip' => $ip,
     ]);
+});
+
+$app->post('/echo', function(Request $request, Response $response, array $args) {
+    $headers = $request->getHeaders();
+    foreach($headers as $name => $value) {
+        $response = $response->withHeader("X-Echo-$name", $value);
+    }
+
+    $types = $request->getHeader('Content-Type');
+    if(isset($types[0])) {
+        $response = $response->withHeader('Content-Type', $types[0]);
+    }
+
+    return $response->write($request->getBody());
 });
 
